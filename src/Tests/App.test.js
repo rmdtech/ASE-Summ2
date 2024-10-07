ce9367109c83ee9c2dce6f67ec20e7c1a2e67259
rmdtech <rmdishman@outlook.com>
2024-10-07 10:47:15 +0100

    Added unit tests for testing the existence of essential UI elements. UI elements being tested will be added at a later date.

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -7,3 +7,60 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('Test suite confirms existence of critical UI elements', () => {
+    test('renders input for "From Currency"', () => {
+        render(<App />);
+        // Check if the element exists using getByTestId
+        const currencyFromSelectElement = screen.getByTestId('currencyFromSelectElement');
+
+        // Assert that the element is in the document
+        expect(currencyFromSelectElement).toBeInTheDocument();
+    });
+
+    test('renders input for "To Currency"', () => {
+        render(<App />);
+        // Check if the element exists using getByTestId
+        const currencyToSelectElement = screen.getByTestId('currencyToSelectElement');
+
+        // Assert that the element is in the document
+        expect(currencyToSelectElement).toBeInTheDocument();
+    });
+
+    test('renders input for Amount Field', () => {
+        render(<App />);
+        // Check if the element exists using getByTestId
+        const amountElement = screen.getByTestId('amountElement');
+
+        // Assert that the element is in the document
+        expect(amountElement).toBeInTheDocument();
+    });
+
+    test('renders Convert Button', () => {
+        render(<App />);
+        // Check if the element exists using getByTestId
+        const convertButtonElement = screen.getByTestId('convertButton');
+
+        // Assert that the element is in the document
+        expect(convertButtonElement).toBeInTheDocument();
+    });
+
+    test('renders Swap Button', () => {
+        render(<App />);
+        // Check if the element exists using getByTestId
+        const swapButtonElement = screen.getByTestId('swapButton');
+
+        // Assert that the element is in the document
+        expect(swapButtonElement).toBeInTheDocument();
+    });
+
+    test('renders Result Field', () => {
+        render(<App />);
+        // Check if the element exists using getByTestId
+        const resultFieldElement = screen.getByTestId('resultField');
+
+        // Assert that the element is in the document
+        expect(resultFieldElement).toBeInTheDocument();
+    });
+
+});
