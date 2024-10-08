b4f70a3fcb95fcca3f4a44d9b05417c70ef08117
rmdtech <rmdishman@outlook.com>
2024-10-08 12:19:29 +0100

    Removes redundant code

--- src/Components/ApiRequestHandler.js
+++ src/Components/ApiRequestHandler.js
@@ -1,21 +1,3 @@
-
-
-export function httpsRequest(apiUrl) {
-    return fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
 export async function httpRequest(apiUrl) {
     try {
         const result = await fetch(apiUrl);
